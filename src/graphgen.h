07484d23dab51035386bd81278b44ce59393c7bf
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2011-08-21 12:50:39 -0400

    improve doc

--- src/graphgen.h
+++ src/graphgen.h
@@ -1,5 +1,5 @@
-#ifndef graph_GENERATORS_H_
-#define graph_GENERATORS_H_
+#ifndef GRAPHGENERATORS_H_
+#define GRAPHGENERATORS_H_
 
 #include <stdbool.h>
 #include <gsl/gsl_rng.h>
@@ -11,25 +11,62 @@
 extern "C" {
 #endif
 
-/** Get a random geometric graph in [0,1]^2 with radius 'r'. */
+/** 
+ * Get a random geometric graph in [0,1]^2 with radius 'r'.
+ * 
+ * 'x' and 'y' must be initialized and the correct amount of space must 
+ * be allocated. The weight of the realized edges is 'r - d', where 'd' 
+ * is the geometric distance between the two vertices (in short the 
+ * weight is always between 0 and 1.0).
+ */
 void graph_get_rgg(graph *g, int vertices, double r, double *x, double *y, gsl_rng *rng);
 
-/** Initialize a graph with a fixed number of vertices. */
+/** 
+ * Get a connected random geometric graph in [0,1]^2 with radius 'r'.
+ * 
+ * 'x' and 'y' must be initialized and the correct amount of space must 
+ * be allocated. The weight of the realized edges is 'r - d', where 'd' 
+ * is the geometric distance between the two vertices (in short the 
+ * weight is always between 0 and 1.0).
+ */
 void graph_get_crgg(graph *g, int vertices, double r, double *x, double *y, gsl_rng *rng);
 
-/** Get a random geometric graph in a rectangle with an area of 1 and with radius 'r'. */
+/**
+ * Get a random geometric graph in a rectangle with an area of 1 and 
+ * radius 'r'.
+ * 
+ * 'x' and 'y' must be initialized and the correct amount of space must 
+ * be allocated. The weight of the realized edges is 'r - d', where 'd' 
+ * is the geometric distance between the two vertices (in short the 
+ * weight is always between 0 and 1.0).
+ */
 void graph_get_rec_rgg(graph *g, int vertices, double width, double r, double *x, double *y, gsl_rng *rng);
 
-/** Initialize a graph with a fixed number of vertices. */
+/**
+ * Get a connected random geometric graph in a rectangle with an area of
+ * 1 and radius 'r'.
+ * 
+ * 'x' and 'y' must be initialized and the correct amount of space must 
+ * be allocated. The weight of the realized edges is 'r - d', where 'd' 
+ * is the geometric distance between the two vertices (in short the 
+ * weight is always between 0 and 1.0).
+ */
 void graph_get_rec_crgg(graph *g, int vertices, double width, double r, double *x, double *y, gsl_rng *rng);
 
-/** Return a complete graph (all vertices linked to each other). */
+/**
+ * Return a complete graph (all vertices linked to each other). Realized
+ * edges have a default weight of 1.0.
+ */
 void graph_get_complete(graph *g, int vertices);
 
-/** Return a circle */
+/**
+ * Return a circle. Realized edges have a default weight of 1.0.
+ */
 void graph_get_circle(graph *g, int vertices);
 
-/** Return a star. */
+/**
+ * Return a star. Realized edges have a default weight of 1.0.
+ */
 void graph_get_star(graph *g, int vertices);
 
 #ifdef __cplusplus
