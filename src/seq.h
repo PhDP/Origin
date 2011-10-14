24bc351b01fb2dcf22d57eb9b7394ccc546b2c55
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2011-10-13 20:08:20 -0400

    doc for seq.h

--- src/seq.h
+++ src/seq.h
@@ -16,6 +16,9 @@
 extern "C" {
 #endif
 
+/**
+ * \brief Type of sequences.
+ */
 typedef enum
 {
     Protein = 0,
@@ -26,88 +29,224 @@ typedef enum
 }
 Sequence_type;
 
+/**
+ * \brief A simple object for storing FASTA sequences.
+ */
 typedef struct
 {
-    char *seq;
-    char *seq_info;
-    int length;
-    Sequence_type type;
+    char *seq; /**< The sequence. */
+
+    char *seq_info; /**< The 'info' on the sequence. */
+
+    unsigned int length; /**< Length of the sequence. */
+
+    Sequence_type type; /**< Type of sequence. */
 }
 Fasta_seq;
 
-/** Deduce the type of a sequence. */
+/**
+ * \brief Deduce the type of a sequence.
+ * 
+ * \param seq    The sequence
+ * \return       The type of sequence.
+ */
 Sequence_type get_type(const char *seq);
 
-/** Extract the nth sequence from a file in fasta format. */
+/**
+ * \brief Extract the nth sequence from a file in fasta format.
+ *
+ * Extract a sequence and store it in a Fasta_seq object.
+ * 
+ * \param filename   Name of the FASTA file.
+ * \param n          Index of the sequence.
+ * \param fasta      A pointer to an unitialized Fasta_seq object.
+ */
 void read_fasta_file(const char *filename, int n, Fasta_seq *fasta);
 
-/** Return 'A', 'T', 'C' or 'G'. */
+/**
+ * \brief Return a random DNA nucleotide.
+ * 
+ * \param rng    A random number generator. 
+ * \return       'A', 'T', 'C', or 'G'.
+ */
 char dna_random_nuc(gsl_rng *rng);
 
-/** Return a random nucleotide with given probabilities (with the probability for C = 1 - prob(A + T + G)). */
+/**
+ * \brief Return a random DNA nucleotide with custom probablity.
+ *
+ * Return a random nucleotide with given probabilities (with the probability for
+ * C = 1 - prob(A + T + G)).
+ * 
+ * \param rng      A random number generator.
+ * \param prob_a   Probability of 'A'.
+ * \param prob_t   Probability of 'T'.
+ * \param prob_g   Probability of 'G'.
+ * \return         'A', 'T', 'C', or 'G'.
+ */
 char dna_random_nuc_prob(gsl_rng *rng, double prob_a, double prob_t, double prob_g);
 
-/** Return a sequence of 'A', 'T', 'C' or 'G'. */
+/**
+ * \brief Return a random DNA sequence.
+ * 
+ * \param rng        A random number generator.
+ * \param seq_size   The length of the resulting sequence.
+ * \return           A sequence of 'A', 'T', 'C', or 'G'.
+ */
 char *dna_random_nuc_seq(gsl_rng *rng, int seq_size);
 
-/** Return 'A', 'U', 'C' or 'G'. */
+/**
+ * \brief Return a random RNA nucleotide.
+ * 
+ * \param rng    A random number generator. 
+ * \return       'A', 'U', 'C', or 'G'.
+ */
 char rna_random_nuc(gsl_rng *rng);
 
-/** Return a random nucleotide with given probabilities (with the probability for C = 1 - prob(A + U + G)). */
+/**
+ * \brief Return a random RNA nucleotide with custom probablity.
+ *
+ * Return a random nucleotide with given probabilities (with the probability for
+ * C = 1 - prob(A + U + G)).
+ * 
+ * \param rng      A random number generator.
+ * \param prob_a   Probability of 'A'.
+ * \param prob_t   Probability of 'U'.
+ * \param prob_g   Probability of 'G'.
+ * \return         'A', 'U', 'C', or 'G'.
+ */
 char rna_random_nuc_prob(gsl_rng *rng, double prob_a, double prob_u, double prob_g);
 
-/** Return a sequence of 'A', 'U', 'C' or 'G'. */
+/**
+ * \brief Return a random RNA sequence.
+ * 
+ * \param rng        A random number generator.
+ * \param seq_size   The length of the resulting sequence.
+ * \return           A sequence of 'A', 'U', 'C', or 'G'.
+ */
 char *rna_random_nuc_seq(gsl_rng *rng, int seq_size);
 
-/** Number of occurences of a char in a sequence. */
+/**
+ * \brief Count the occurences of a char in a string.
+ * 
+ * \param seq    A sequence. 
+ * \return       The number of occurence.
+ */
 unsigned int seq_count(const char *seq);
 
-/** Number of adenosine 'A' in the sequence. */
+/**
+ * \brief Count the number of adenosine 'A' in the sequence.
+ * 
+ * \param seq    A sequence. 
+ * \return       The number of adenosine 'A'.
+ */
 unsigned int a_count(const char *seq);
 
-/** Number of thymidine 'T' in the sequence. */
+/**
+ * \brief Count the number of thymidine 'T' in the sequence.
+ * 
+ * \param seq    A sequence. 
+ * \return       The number of thymidine 'T'.
+ */
 unsigned int t_count(const char *seq);
 
-/** Number of uracil 'U' in the sequence. */
+/**
+ * \brief Count the number of uracil 'U' in the sequence.
+ * 
+ * \param seq    A sequence. 
+ * \return       The number of uracil 'U'.
+ */
 unsigned int u_count(const char *seq);
 
-/** Number of guanine 'G' in the sequence. */
+/**
+ * \brief Count the number of guanine 'G' in the sequence.
+ * 
+ * \param seq    A sequence. 
+ * \return       The number of guanine 'G'.
+ */
 unsigned int g_count(const char *seq);
 
-/** Number of cytosine 'C' in the sequence. */
+/**
+ * \brief Count the number of cytosine 'C' in the sequence.
+ * 
+ * \param seq    A sequence. 
+ * \return       The number of cytosine 'C'.
+ */
 unsigned int c_count(const char *seq);
 
-/** Number of cytosine 'C' and guanine 'G' in the sequence. */
+/**
+ * \brief Count the number of cytosine 'C' and guanine 'G' in the sequence.
+ * 
+ * \param seq    A sequence. 
+ * \return       The number of cytosine 'C' and guanine 'G'.
+ */
 unsigned int gc_count(const char *seq);
 
 /** Return GC content (number of 'G' + number of 'C') / size. */
 double gc_content(const char *seq);
 
-/** Return 'TRUE' if the sequence is only made of 'G', 'C', 'T' or 'A'. */
+/**
+ * \brief Return TRUE if the sequence is only made of 'G', 'C', 'T' or 'A'.
+ * 
+ * \param dna_seq    A DNA sequence. 
+ * \return           1 (TRUE) if the sequence is made of 'G', 'C', 'T' or 'A'.
+ */
 int dna_pure_seq(const char *dna_seq);
 
-/** Return 'TRUE' if the sequence is only made of 'G', 'C', 'U' or 'A'. */
+/**
+ * \brief Return TRUE if the sequence is only made of 'G', 'C', 'U' or 'A'.
+ * 
+ * \param rna_seq    A RNA sequence. 
+ * \return           1 (TRUE) if the sequence is made of 'G', 'C', 'U' or 'A'.
+ */
 int rna_pure_seq(const char *rna_seq);
 
-/** Return a sequence with everything removed except 'G', 'C', 'T' and 'A'. */
+/**
+ * \brief Return a sequence with everything removed except 'G', 'C', 'T' and 'A'.
+ * 
+ * \param dna_seq    A DNA sequence. 
+ * \return           A DNA sequence without the ambiguities.
+ */
 char *dna_rmv_amb(char *dna_seq);
 
-/** Return a sequence with everything removed except 'G', 'C', 'U' and 'A'. */
+/**
+ * \brief Return a sequence with everything removed except 'G', 'C', 'U' and 'A'.
+ * 
+ * \param rna_seq    A RNA sequence. 
+ * \return           A RNA sequence without the ambiguities.
+ */
 char *rna_rmv_amb(char *rna_seq);
 
-/** Return the antisense stand of a DNA sequence. */
+/**
+ * \brief Return the antisense strand of a DNA sequence.
+ * 
+ * \param dna_seq    A DNA sequence. 
+ * \return           The antisense strand.
+ */
 char *dna_antisense(const char *dna_seq);
 
-/** DNA -> RNA. Start reading when encountering a start codon. Both the DNA sequence and the resulting RNA sequence are in the 5' -> 3' direction. */
+/**
+ * \brief DNA -> RNA.
+ *
+ * Start reading when encountering a start codon. Both the DNA sequence and the
+ * resulting RNA sequence are in the 5' -> 3' direction.
+ * 
+ * \param dna_seq    A DNA sequence. 
+ * \return           RNA sequence resulting from transcription.
+ */
 char *transcription(const char *dna_seq);
 
-/** Translate RNA to an amino acid sequence. */
+/**
+ * \brief Translate RNA to an amino acid sequence.
+ *
+ * \param dna_seq    A RNA sequence. 
+ * \return           A sequence of amino acids.
+ */
 char *translation(const char *rna_seq);
 
-/** Return true if the char is a standard DNA nucleotide. */
+/** \brief Return true if the char is a standard DNA nucleotide. */
 #define DNANUC(c) (c=='A'||c=='T'||c=='G'||c=='C')
 
-/** Return true if the char is a standard RNA nucleotide. */
+/** \brief Return true if the char is a standard RNA nucleotide. */
 #define RNANUC(c) (c=='A'||c=='U'||c=='G'||c=='C')
 
 #ifdef __cplusplus
