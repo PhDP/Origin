202aee044f84fb4522e11dafdedf6b5406dc6d24
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2011-10-14 08:45:19 -0400

    treenode doc

--- src/treenode.h
+++ src/treenode.h
@@ -32,37 +32,92 @@ typedef struct tnode_
 }
 tnode;
 
-/** Initialize a tnode object. */
+/**
+ * \brief Initialize a tree object.
+ * 
+ * \param p       Pointer to the parent.
+ * \param name    Name of the node.
+ * \param data    The data inside the node.
+ * \return        A pointer to the object.
+ */
 tnode *tnode_init(tnode *p, char *name, void *data);
 
-/** Set the children of the node. */
-void tnode_set_children(tnode *t, tnode *l, tnode *r); // TODO: Replace by a define (and test!)
+/**
+ * \brief Set the children of the node.
+ *
+ * \param t     The node to modify.
+ * \param l     Left child.
+ * \param r     Right child.
+ */
+void tnode_set_children(tnode *t, tnode *l, tnode *r);
 
-/** Number of edges in the subtree. */
-int tnode_numedges(tnode *t);
+/**
+ * \brief Number of edges in the subtree.
+ *
+ * \param t    The subtree to analyze.
+ * \return     The number of edges in the subtree.
+ */
+unsigned int tnode_numedges(tnode *t);
 
-/** Number of leaves in the subtree. */
-int tnode_numleaves(tnode *t);
+/**
+ * \brief Number of leaves in the subtree.
+ *
+ * \param t    The subtree to analyze.
+ * \return     The number of leaves in the subtree.
+ */
+unsigned int tnode_numleaves(tnode *t);
 
-/** Number of nodes between this node and the root */
-int tnode_toroot(tnode *t);
+/**
+ * \brief Number of nodes between this node and the root.
+ *
+ * \param t    The subtree to analyze.
+ * \return     Number of nodes between this node and the root.
+ */
+unsigned int tnode_toroot(tnode *t);
 
 /** Check if the subtree is really a strictly binary tree. */
 int tnode_sbinary(tnode *t);
 
-/** Return 'true' if the node is a leaf. */
+/**
+ * \brief  Return 'true' if the node is a leaf.
+ * 
+ * \param t    The subtree to analyze.
+ * \return     1 (TRUE) if the node is a leaf, 0 (FALSE) otherwise.
+ */
 int tnode_leaf(tnode *t);
 
-/** Return 'true' if the node is a root. */
+/**
+ * \brief  Return 'true' if the node is a root.
+ *
+ * \param t    The subtree to analyze.
+ * \return     1 (TRUE) if the node is the root, 0 (FALSE) otherwise.
+ */
 int tnode_root(tnode *t);
 
-/** Return 'true' if the node is an internal node. */
+/**
+ * \brief  Return 'true' if the node is an internal node.
+ *
+ * \param t    The subtree to analyze.
+ * \return     1 (TRUE) if the node is internal, 0 (FALSE) otherwise.
+ */
 int tnode_internal(tnode *t);
 
-/** Return the simple newick tree. */
+/**
+ * \brief Return a the tree as a string in Newick format.
+ *
+ * \param t    The root of the tree used to build the string.
+ * \return     A string in Newick format.
+ */
 char *tnode_simple_newick(tnode *t);
 
-/** Recursively free the memory of the node. The tree node doesn't own the data at the end of the void pointer so the user has to free this memory manually (or reimplement this function). */
+/**
+ * \brief Recursively free the memory of the node.
+ *
+ * The tree node doesn't own the data at the end of the void pointer so the user
+ * has to free this memory manually (or reimplement this function).
+ * 
+ * \param t    The root of the tree to free.
+ */
 void tnode_free(tnode *t);
 
 #ifdef __cplusplus
