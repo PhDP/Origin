ecdbc677741ea8aed495ef2d50aaad4bb7db1cd3
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2011-11-25 14:58:18 -0500

    improve build file

--- src/build.py
+++ src/build.py
@@ -1,17 +1,40 @@
 #! /usr/bin/env python
 
+# A small Python script to compile the programs.
+#
+# Yes I know about make (and Waf), and I understand the limitations of this simple script, but
+# I prefer this system. Bite me :P
+#
+# Currently the script takes only one argument: the name of the compiler.
+
 import os
 import sys
 
+# If the name of the compiler isn't supplied, use gcc:
 if len(sys.argv) is 2:
     compiler = sys.argv[1]
 else:
-    compiler = "gcc"
+    compiler = 'gcc'
 
-name = "ssne"
-cflags = "-O3 -DHAVE_INLINE -DOINLINING -std=c99"
-cfiles = "model_ssne.c species.c dvector.c ivector.c specieslist.c graph.c graphgen.c utils.c ecology.c stats.c maths.c str.c"
-clinks = "-lgsl -lgslcblas -lm -lpthread"
-s = compiler + " " + cflags + " " + cfiles + " -o " + name + " " + clinks
+# Name of the program:
+name = 'ssne'
+
+# Flags:
+cflags = '-O3 -DHAVE_INLINE -DOINLINING -DEXTRAPRINT -std=c99'
+
+# List of files to compile:
+cfiles = 'model_ssne.c species.c dvector.c ivector.c specieslist.c graph.c graphgen.c utils.c ecology.c stats.c maths.c str.c'
+
+# Linkage:
+clinks = '-lgsl -lgslcblas -lpthread'
+if compiler != 'icc':
+    clinks += ' -lm'
+
+# The final command to compile:
+s = compiler + ' ' + cflags + ' ' + cfiles + ' -o ' + name + ' ' + clinks
+
+# Show the command used:
 print(s)
+
+# Compile:
 os.system(s)
--- src/vvector.h
+++ src/vvector.h
@@ -63,4 +63,3 @@ void vvector_free(vvector *v);
 #endif
 
 #endif
-
